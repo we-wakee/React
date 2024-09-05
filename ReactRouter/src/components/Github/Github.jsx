905009aa1248aa597171f0be7c552925ddf5c49c
Vivek Aryan <varyanlucky@gmail.com>
2024-09-05 06:31:04 +0530

    first commit

---
+++ ReactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+ import {Link,NavLink} from 'react-router-dom'
+
+ function Github(){
+
+    return(
+
+        <div>
+             <Link>
+          
+                    <h1>Github ki occhav</h1>
+            </Link>
+
+        </div>
+       
+       
+    )
+ }
+ 
+ export default Github
---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import AddToDo from './components/AddToDo'
+import ToDos from './components/ToDos'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+      <AddToDo />
+      <ToDos />
+    </>
+  )
+}
+
+export default App
